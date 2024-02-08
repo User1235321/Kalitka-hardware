84a15a9274064f771772f4b68a76f3307b6519e2
User1235321 <pavreb1235321@gmail.com>
2024-02-08 18:43:12 +0300

    Now master can read switch any byte

--- master/master.ino
+++ master/master.ino
@@ -4,19 +4,19 @@
 #define RST_PIN 9
 #define SS_PIN 10
 
-MFRC522 rfid(SS_PIN, RST_PIN);   // Объект rfid модуля
-MFRC522::MIFARE_Key key;         // Объект ключа
+MFRC522 rfid(SS_PIN, RST_PIN);
+MFRC522::MIFARE_Key key;
 
-int numOfDoor = 0;
-uint32_t rebootTimer = millis();
+uint32_t rebootTimer;
+long mode;
+long numOfDoor;
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
   SPI.begin();
   rfid.PCD_Init();
-
-  rfid.PCD_SetAntennaGain(rfid.RxGain_max);  // Установка усиления антенны
-
+  rfid.PCD_SetAntennaGain(rfid.RxGain_max);
   rfid.PCD_AntennaOff();
   rfid.PCD_AntennaOn();
 
@@ -24,44 +24,75 @@ void setup() {
   {
     key.keyByte[i] = 0xFF;
   }
+
+  rebootTimer = millis();
+  mode = 0;
+  numOfDoor = 0;
 }
 
-void loop() {
-  if (numOfDoor == -1)
-  {
-    numOfDoor = Serial.parseInt();
-  }
-
+void loop()
+{
+  //rfid reboot
   if ((millis() - rebootTimer) >= 1000)
   {
     rebootTimer = millis();
-
-    digitalWrite(RST_PIN, HIGH);          // Сбрасываем модуль
+    digitalWrite(RST_PIN, HIGH);
     delayMicroseconds(2);
-    digitalWrite(RST_PIN, LOW);           // Отпускаем сброс
+    digitalWrite(RST_PIN, LOW);
     rfid.PCD_Init();
   }
 
-  if (rfid.PICC_IsNewCardPresent() && rfid.PICC_ReadCardSerial())  // метка поднесена и читается
+  mode = (mode == 0) ? Serial.parseInt() : mode;
+  if ((mode == 1) || (mode == 2))
   {
-    if (rfid.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, ((((numOfDoor / 4) + 1) * 4) - 1), &key, &(rfid.uid))
-        == MFRC522::STATUS_OK)
-    {
-      uint8_t dataToWrite[16];
-      for (int i = 0; i < 16; ++i)
-      {
-        dataToWrite[0] = 1;
-      }
+    numOfDoor = (numOfDoor == 0) ? Serial.parseInt() : numOfDoor;
 
-      if (rfid.MIFARE_Write(numOfDoor, dataToWrite, 16) != MFRC522::STATUS_OK)
+    if (numOfDoor == 0) return; //Number of door isn't read
+    if (!rfid.PICC_IsNewCardPresent() || !rfid.PICC_ReadCardSerial()) return; // Can't read cart
+    long blockNum = (1 + numOfDoor / 16) + ((1 + numOfDoor / 16) / 3);
+    if (rfid.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, ((((blockNum / 4) + 1) * 4) - 1), &key, &(rfid.uid))
+      != MFRC522::STATUS_OK)
+    {
+      Serial.println("Key error!");
+      return;
+    }
+
+    uint8_t dataBlock[18];
+    uint8_t size = sizeof(dataBlock);
+    if (rfid.MIFARE_Read(blockNum, dataBlock, &size) != MFRC522::STATUS_OK)
+    {
+      Serial.println("Read error");
+      return;
+    }
+
+    if (mode == 1)
+    {
+      //reading
+      Serial.print("Door number ");
+      Serial.print(numOfDoor);
+      Serial.print(" has status: ");
+      Serial.println(dataBlock[(numOfDoor % 16)]);
+      rfid.PICC_HaltA();                              // Завершаем работу с меткой
+      rfid.PCD_StopCrypto1();
+      mode = 0;
+      numOfDoor = 0;
+    }
+    else if (mode == 2)
+    {
+      //writing
+      dataBlock[numOfDoor % 16] = 1 - dataBlock[numOfDoor % 16];
+      if (rfid.MIFARE_Write(blockNum, dataBlock, 16) != MFRC522::STATUS_OK)
       {
         Serial.println("Bad write");
       }
       else
       {
-        Serial.println("Good write");
+        Serial.print(numOfDoor);
+        Serial.print(" now is ");
+        Serial.println(dataBlock[numOfDoor % 16]);
         rfid.PICC_HaltA();
-        numOfDoor = -1;
+        mode = 0;
+        numOfDoor = 0;
       }
     }
   }
